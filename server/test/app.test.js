e34e7361a6b01acf45472bbedb5aa56943904f25
otitoju <otitojuoluwapelumi@gmail.com>
2020-07-19 12:09:30 +0100

    refactor test

--- server/test/app.test.js
+++ server/test/app.test.js
@@ -2,92 +2,92 @@ const chai = require('chai')
 const chaiHttp = require('chai-http')
 chai.use(chaiHttp);
 chai.should();
-const port = "http://localhost:4001"
+const port = "http://localhost:4000"
 
 describe("POST TESTING", () => {
-    // describe("GET /api/v1/posts", () => {
-    //     // Test to get all posts
-    //     it("should get all posts", (done) => {
-    //          chai.request(port)
-    //              .get('/api/v1/posts')
-    //              .end((err, success) => {
-    //                  success.should.have.status(200);
-    //                  success.body.should.be.a('object');
-    //                  console.log(success.statusCode)
-    //                  console.log(success.body.success)
-    //                  done();
-    //             });
-    //      });
+    describe("GET /api/v1/posts", () => {
+        // Test to get all posts
+        it("should get all posts", (done) => {
+             chai.request(port)
+                 .get('/api/v1/posts')
+                 .end((err, success) => {
+                     success.should.have.status(200);
+                     success.body.should.be.a('object');
+                     console.log(success.statusCode)
+                     console.log(success.body.success)
+                     done();
+                });
+         });
 
-    //      it("should get a single post", (done) => {
-    //         let id = "5f0d72e6d04dda2b081ed67b"
-    //         chai.request(port)
-    //             .get(`/api/v1/post/${id}`)
-    //             .end((err, success) => {
-    //                 success.should.have.status(200);
-    //                 success.body.should.be.a('object');
-    //                 console.log(success.statusCode)
-    //                 console.log(success.body.success)
-    //                 done();
-    //            });
-    //     });
+         it("should get a single post", (done) => {
+            let id = "5f0d72e6d04dda2b081ed67b"
+            chai.request(port)
+                .get(`/api/v1/post/${id}`)
+                .end((err, success) => {
+                    success.should.have.status(200);
+                    success.body.should.be.a('object');
+                    console.log(success.statusCode)
+                    console.log(success.body.success)
+                    done();
+               });
+        });
 
-    // });
+    });
 
-    // describe("POST /api/v1/post/create/:authorId", () => {
-    //     it("should not create new post without valid author id", (done) => {
-    //         let authorId = "5f0d72e6d04dda2b081ed67b"
-    //         let posts = {
-    //             title: "unit testing",
-    //             content: "unit testing 1"
-    //         }
-    //         chai.request(port)
-    //             .post(`/api/v1/post/create/${authorId}`)
-    //             .send(posts)
-    //             .end((err, success) => {
-    //                 success.should.have.status(400)
-    //                 success.body.should.be.a('object')
-    //                 console.log(success.statusCode)
-    //                 console.log(success.body.success)
-    //                 done()
-    //             })
-    //     })
+    describe("POST /api/v1/post/create/:authorId", () => {
+        it("should not create new post without valid author id", (done) => {
+            let authorId = "5f0d72e6d04dda2b081ed67b"
+            let posts = {
+                title: "unit testing",
+                content: "unit testing 1"
+            }
+            chai.request(port)
+                .post(`/api/v1/post/create/${authorId}`)
+                .send(posts)
+                .end((err, success) => {
+                    success.should.have.status(400)
+                    success.body.should.be.a('object')
+                    console.log(success.statusCode)
+                    console.log(success.body.success)
+                    done()
+                })
+        })
 
-    //     it("should not create new post without complete data", (done) => {
-    //         let authorId = "5f1159fbce0efe211cfa82ed"
-    //         let posts = {
-    //             title: "unit testing"
-    //         }
-    //         chai.request(port)
-    //             .post(`/api/v1/post/create/${authorId}`)
-    //             .send(posts)
-    //             .end((err, success) => {
-    //                 success.should.have.status(400)
-    //                 success.body.should.be.a('object')
-    //                 console.log(success.statusCode)
-    //                 console.log(success.body.success)
-    //                 done()
-    //             })
-    //     })
+        it("should not create new post without complete data", (done) => {
+            let authorId = "5f1159fbce0efe211cfa82ed"
+            let posts = {
+                title: "unit testing"
+            }
+            chai.request(port)
+                .post(`/api/v1/post/create/${authorId}`)
+                .send(posts)
+                .end((err, success) => {
+                    success.should.have.status(400)
+                    success.body.should.be.a('object')
+                    console.log(success.statusCode)
+                    console.log(success.body.success)
+                    done()
+                })
+        })
 
-    //     it("should create new post without complete data", (done) => {
-    //         let authorId = "5f1159fbce0efe211cfa82ed"
-    //         let posts = {
-    //             title: "unit testing 1",
-    //             content: "Unit one testing by author1"
-    //         }
-    //         chai.request(port)
-    //             .post(`/api/v1/post/create/${authorId}`)
-    //             .send(posts)
-    //             .end((err, success) => {
-    //                 success.should.have.status(201)
-    //                 success.body.should.be.a('object')
-    //                 console.log(success.statusCode)
-    //                 console.log(success.body.success)
-    //                 done()
-    //             })
-    //     })
-    // })
+        it("should create new post without complete data", (done) => {
+            let authorId = "5f1159fbce0efe211cfa82ed"
+            let posts = {
+                title: "unit testing 1",
+                content: "Unit one testing by author1"
+            }
+            chai.request(port)
+                .post(`/api/v1/post/create/${authorId}`)
+                .send(posts)
+                .end((err, success) => {
+                    success.should.have.status(201)
+                    success.body.should.be.a('object')
+                    console.log(success.statusCode)
+                    console.log(success.body.success)
+                    done()
+                })
+        })
+    })
 
     describe("UPDATE POST /api/v1/update/post/:id", () => {
         it("should not update post if no valid id", (done) => {
@@ -108,22 +108,22 @@ describe("POST TESTING", () => {
                 })
         })
 
-        // it("should update post with valid id", (done) => {
-        //     let id = "5f115e74ce0efe211cfa82f3"
-        //     let update_post = {
-        //         title: "unit testing 1 updated"
-        //     }
-        //     chai.request(port)
-        //         .put(`/api/v1/update/post/${id}`)
-        //         .send(update_post)
-        //         .end((err, success) => {
-        //             success.should.have.status(200)
-        //             success.body.should.be.a('object')
-        //             console.log(success.statusCode)
-        //             console.log(success.body.success)
-        //             done()
-        //         })
-        // })
+        it("should update post with valid id", (done) => {
+            let id = "5f115e74ce0efe211cfa82f3"
+            let update_post = {
+                title: "unit testing 1 updated"
+            }
+            chai.request(port)
+                .put(`/api/v1/update/post/${id}`)
+                .send(update_post)
+                .end((err, success) => {
+                    success.should.have.status(200)
+                    success.body.should.be.a('object')
+                    console.log(success.statusCode)
+                    console.log(success.body.success)
+                    done()
+                })
+        })
     })
 
     describe("DELETE POST /api/v1/delete/post/:id", () => {
@@ -140,17 +140,17 @@ describe("POST TESTING", () => {
                 })
         })
 
-        // it("should delete if id is valid", (done) => {
-        //     let id = "5f0d7396d04dda2b081ed67c"
-        //     chai.request(port)
-        //         .delete(`/api/v1/delete/post/${id}`)
-        //         .end((err, success) => {
-        //             success.should.have.status(200)
-        //             success.body.should.be.a('object')
-        //             console.log(success.statusCode)
-        //             console.log(success.body.success)
-        //             done()
-        //         })
-        // })
+        it("should delete if id is valid", (done) => {
+            let id = "5f0d7396d04dda2b081ed67c"
+            chai.request(port)
+                .delete(`/api/v1/delete/post/${id}`)
+                .end((err, success) => {
+                    success.should.have.status(200)
+                    success.body.should.be.a('object')
+                    console.log(success.statusCode)
+                    console.log(success.body.success)
+                    done()
+                })
+        })
     })
 })
